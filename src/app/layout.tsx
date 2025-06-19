ce7bcf4dc05007168e12c79c0d64300adb27c5eb
Eric Barbosa Jr <ericbarbosajr@gmail.com>
2025-06-19 17:54:50 -0600

    Refactor layout to import SidebarNav from the new _components directory, enhancing project structure and organization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SidebarNav from "./SidebarNav";
+import SidebarNav from "./_components/SidebarNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
